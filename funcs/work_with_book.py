06b9fd842963ffbf6fcd46c227d0184b786e238a
Pavlov Ivan <pivaq4530@gmail.com>
2025-06-06 11:37:36 +0300

    Main work is done

--- funcs/work_with_book.py
+++ funcs/work_with_book.py
@@ -1,6 +1,7 @@
 from aiogram import Bot
 from aiogram.types import Message
 from config.config import load_config
+from config.data import book_page
 
 config = load_config()
 bot: Bot = Bot(token=config.tg_bot.token)
@@ -10,6 +11,8 @@ async def download_book(msg: Message) -> None:
     file = await bot.get_file(msg.document.file_id)
     file_path: str = file.file_path
     await bot.download_file(file_path, 'books/' + msg.document.file_name)
+    book_page[msg.document.file_name] = 1
+
 
 
 
@@ -22,5 +25,7 @@ def scan_the_book(file_name: str) -> str:
                 return f.read()
 
 
-def read_the_book(book_itself):
-    return book_itself[:1000]
+def read_the_book(book_itself, page=1):
+    return book_itself[page * 500 - 500: page * 500]
+
+
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,16 +1,18 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from funcs.work_with_book import scan_the_book, download_book, read_the_book
 from funcs.filtres import FormatFile
 from lexicon.lexica import help_msg
 from keyboards.menu import  menu_board
-from keyboards.keyboard_book import book_keyboard
+from keyboards.page_change import page_board
+from config.data import book_page
 
 
 
 router = Router()
-
+book = ''
+book_name = ''
 
 @router.message(Command(commands='start'))
 async def process_start(msg: Message):
@@ -24,10 +26,23 @@ async def give_help(msg: Message):
 
 @router.message(F.document, FormatFile())
 async def doc_handler(msg: Message):
+    global book, book_name
     await download_book(msg)
+    book_name = msg.document.file_name
     book = scan_the_book(msg.document.file_name)
-    await msg.reply(read_the_book(book))
+    await msg.reply(read_the_book(book), reply_markup=page_board)
 
+
+@router.callback_query(F.data == 'next')
+async def next_page(clb: CallbackQuery):
+    book_page[book_name] += 1
+    await clb.message.edit_text(read_the_book(book, book_page[book_name]), reply_markup=page_board)
+
+
+@router.callback_query(F.data == 'back')
+async def back_page(clb: CallbackQuery):
+    book_page[book_name] -= 1
+    await clb.message.edit_text(read_the_book(book, book_page[book_name]), reply_markup=page_board)
 
 @router.message()
 async def wrong_move(msg: Message):
